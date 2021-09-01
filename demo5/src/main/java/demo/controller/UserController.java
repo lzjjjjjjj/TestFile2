652a87e54c91cdb6127e34b822dc729f7dca49a9
lzjjjjjjj <1247708217@qq.com>
2021-09-01 21:21:13 +0800

    lzj

--- demo5/src/main/java/demo/controller/UserController.java
+++
@@ -1,4 +0,0 @@
-package demo.controller;
-
-public class UserController {
-}
--- demo5/src/main/java/demo/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package demo.service;
-
-import demo.pojo.User;
-
-import java.util.List;
-
-public interface UserService {
-    List<User> list();
-}
